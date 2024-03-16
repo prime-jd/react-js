3e3fb789ab3d7f5a7a40bf0826b3f875816eacc0
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-03-16 10:33:26 +0000

    custom jsx

---
+++ vitejs-vite-dob6z5/src/custom.jsx
@@ -0,0 +1,10 @@
+// jsx is the extended  vesion javascript providing it markup language property
+// we can say it as js + XML
+// in jsx we can create desired html element by describing there functionalities
+
+
+function custom(){                    // this function 
+    return(
+        <h1>Ravi Shankar Tripathi</h1>
+    )
+}
