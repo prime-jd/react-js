7c1a72927622bb054f88ce252154fc4af9e7b962
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-03-16 10:32:54 +0000

    app.jsx

--- vitejs-vite-dob6z5/src/App.jsx
+++ vitejs-vite-dob6z5/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
+function App() {                
   return (
     <>
+    <h1>React js</h1>
       <p>My first react project</p>
     </>
   );
 }
 
 export default App;
+
