f6a7e23bd885bc10687e23e7e9eb1619680eec92
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-03-18 17:53:20 +0000

    ..

--- 03_viteReact/src/App.jsx
+++ 03_viteReact/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-600 p-4 rounded-lg hover:bg-blue-700 '>React With Tailwind</h1>
+      <p className='bg-green-600 p-4 rounded-lg hover:bg-blue-700 m-4 '>React With Tailwind</p>
+      <Card />
     </>
   )
 }
--- 03_viteReact/src/card.jsx
+++ 03_viteReact/src/card.jsx
@@ -1,1 +1,20 @@
 // adding cards
+function Card(){
+    return(
+        <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpf54XPulJI5v_5q-N2UCynDtu9ZLzhbqgNBZyv79ccuk0Jw4A&s" alt="Woman's Face" />
+  <div className="text-center space-y-2 sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg text-black font-semibold">
+        Erin Lindford
+      </p>
+      <p className="text-slate-500 font-medium">
+        Product Engineer
+      </p>
+    </div>
+    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+  </div>
+</div>
+    )
+}
+export default Card
