04ac26c06dcb714390daa49322415d6b342d8a26
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-03-19 14:14:47 +0000

    background color changer

--- 04_viteReact/src/bg_changer.jsx
+++ 04_viteReact/src/bg_changer.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react"
+import React, { useState } from 'react';
 
-function Bg_chg(){
-    const [color,setColor] = useState("olive")
-    
-    return(
-        <>
-        <div className="bg-white rounded-3xl px-4 justify-center hover:bg-amber-300 ">
-        <button onClick={()=>setColor("red") } className="m-4 bg-red-600 rounded-3xl border-black" style={{backgroundColor : "red"}}>Red</button>
-        <button className="bg-green-400 rounded-3xl m-4 border-black">Green</button>
-        <button className="bg-blue-400 rounded-3xl m-4 border-black">Blue</button>
+function Bg_chg() {
+    const [color, setColor] = useState("olive");
+
+    return (
+    <>
+      <div className='fixed inset-0 overflow-hidden' style={{backgroundColor : color}} >
+        <div className={`flex  rounded-3xl px-2 justify-center`}>
+            <button onClick={() => setColor("red")} className="outline-none m-2 bg-red-600 rounded-3xl border-black" style={{backgroundColor: "red"}}>Red</button>
+            <button onClick={() => setColor("green")} className="outline-none bg-green-400 rounded-3xl m-2 border-black" style={{backgroundColor:"green"}}>Green</button>
+            <button onClick={() => setColor("blue")} className="outline-none bg-blue-400 rounded-3xl m-2 border-black" style={{backgroundColor: "blue"}}>Blue</button>
         </div>
-        </>
-    )
+      </div>
+    </>
+    );
 }
-export default Bg_chg
+
+export default Bg_chg;
