78218ccc9944cb355b749eecbc04526460734325
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-03-21 21:09:25 +0000

    ...

--- 05_viteReact/src/pass_gen.jsx
+++ 05_viteReact/src/pass_gen.jsx
@@ -1,35 +1,67 @@
-import  { useState, useEffect, useCallback } from 'react';
+import  { useState, useEffect, useCallback, useRef } from 'react';
 
 
 function Pass_gen(){
-    let [password, setPassword]= useState("xyz")
+    let [password, setPassword]= useState(" ")
     let [number, setNumber]= useState(false)
     let [character, setCharacter]= useState(false)
-    let [length, setLength]= useState("4")
+    let [length, setLength]= useState(4)
+    const passwordRef = useRef(null)
 
-    let gen_pass = useCallback(()=> {
+    const gen_pass = useCallback(()=> {
+      let pass = ""
         let ch = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
         let num = "1234567890"
         let spCh = "!@#$%^&*()_+<>?/}{][|"
-        if(number==true)
+        if(number)
           ch += num;
-        if(character==true)
+        if(character)
           ch += spCh;
         for (let i = 0; i < length; i++) {
             let x= Math.floor(Math.random()*ch.length + 1);
-            password += ch[x];
+            pass += ch.charAt(x);
         }
-        setPassword(password);
-    })[length, number, character];
+        setPassword(pass);
+    },[length, number, character,setPassword])
 
-    useEffect(()=> { gen_pass},
-    [length, number,character]);
+    const copyPasswordToClipboard = useCallback(() => {
+      passwordRef.current?.select();
+      passwordRef.current?.setSelectionRange(0, 999);
+      window.navigator.clipboard.writeText(password)
+    }, [password])
+
+    useEffect(()=> { gen_pass() },
+    [length, number, character ,gen_pass])
+
     return(
-        <div className='border-2 p-6 bg-gray-400 border-black border-solid w-full h-36'>
-          <input type='text' className=' border-2 border-black'></input>
-          <button className=''>Copy</button>
+      <>
+        <div className='border-2 p-6 bg-gray-200 border-black border-solid w-full h-46'>
+          <h1 className='my-5 text-green-600'><u>PASSWORD GENERATOR</u></h1>
+          <div className='flex'>
+          <input type='text' placeholder='password' value={password} className=' border-2 border-black w-full px-2' onClick={copyPasswordToClipboard} readOnly />
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+          </div>
+          <div className='flex'>
+          <div className='flex m-5'>
+            <input type='range' className='cursor-pointer' onChange={(e)=>setLength(e.target.value)} min={0} max={100} value={length}></input>
+            <label>&nbsp;length:{length}<span></span></label>
+          </div>
+            <div className='mx-9 my-5'>
+              <input type='Checkbox' onChange={() => {
+                  setCharacter((prev) => !prev )
+              }}></input>
+              <label>&nbsp;Character &nbsp;</label>
+              <input type='Checkbox' onChange={() => {
+                  setNumber((prev) => !prev )
+              }}></input>
+              <label>&nbsp;Number</label>
+           </div> 
+          </div>
+               
         </div>
-
+        <button className='justify-centre m-5 text-white border-black border-2 p-2 bg-blue-500'>Refresh</button>
+        </>
+      
     )
 }
 export default Pass_gen
