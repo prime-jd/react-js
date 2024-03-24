9cc208341ddc62345c3dd15f2c0f1d243ca08bf6
prime-jd <120632610+prime-jd@users.noreply.github.com>
2024-03-24 17:38:05 +0000

    React Routers

--- 07_viteReact/src/main.jsx
+++ 07_viteReact/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from './components/Home/Home.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import About from './components/About/About.jsx'
+
+const router = createBrowserRouter(
+   createRoutesFromElements(
+    <Route path='/' element = {<Layout />} >
+      <Route path='' element = {<Home />} />
+      <Route path='about' element = {<About />} />
+      <Route path='contact' element = {<Contact />} />
+    </Route>
+   )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
