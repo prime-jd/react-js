2a384ded98accf3383a8a8bc110905fdcc937379
prime_jd <ravishankartripathi2002@gmail.com>
2024-03-27 17:49:49 +0530

    project

--- 09_viteReact/src/main.jsx
+++ 09_viteReact/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout />}>
+      <Route path='' element= {<Home />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
+  </React.StrictMode>
 )
