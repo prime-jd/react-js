f1d895bfdc4259434f7cd08d52baddca3b83fb6a
prime_jd <ravishankartripathi2002@gmail.com>
2024-03-30 20:02:51 +0530

    Todo Context Project

--- 11_viteReact/src/App.jsx
+++ 11_viteReact/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import { TodoProvider } from './Context/TodoContext'
+import { TodoProvider } from './context/TodoContext'
 import './App.css'
-import TodoForm from './Components/TodoForm'
-import TodoItem from './Components/TodoItem'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
