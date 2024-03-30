25808cd20d36be74d997dea5533f823d4637ea59
prime_jd <ravishankartripathi2002@gmail.com>
2024-03-30 14:54:42 +0530

    ..

--- 11_viteReact/src/App.jsx
+++ 11_viteReact/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { UserContextProvider } from './Context/TodoContext'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [todos, setTodo] = useState([])
+
+  useEffect(()=>{},[])
+
+  const addTodo = (todo)=>{
+    setTodo((prev)=>[{id : Date.now(), ...todo}, ...prev])
+  }
+
+  const updateTodo = (id, todo)=> {
+    setTodo((prev)=>prev.map((prevTodo)=>(prevTodo.id==id)? todo:prevTodo))
+  }
+
+  const deleteTodo = (id)=>{
+    setTodo((prev)=> prev.filter((prevTodo)=>(prevTodo.id !== id)))
+  }
+
+  const toggleTodo = (id)=>{
+    setTodo((prev)=>prev.map((prevTodo)=>(prevTodo.id===id)? {...prev, completed :!prevTodo}: prevTodo))
+  }
+  
 
   return (
-    <>
+    <UserContextProvider value={{todos, addTodo,updateTodo,deleteTodo,toggleTodo}}>
       
-    </>
+    </UserContextProvider>
   )
 }
 
--- 11_viteReact/src/Context/TodoContext.js
+++ 11_viteReact/src/Context/TodoContext.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { useContext } from "react";
 
-export const UserContext = React.createContext({})
+export const UserContext = React.createContext({
+    todos : {
+        id : 1,
+        data : "todoMsg",
+        completed : false
+    },
+    addTodo : (todo)=>{},
+    updateTodo : (id,todo)=>{},
+    deleteTodo : (id)=>{},
+    toggleTodo : (id)=>{}
+})
 
 export const UserContextProvider = UserContext.Provider
 
