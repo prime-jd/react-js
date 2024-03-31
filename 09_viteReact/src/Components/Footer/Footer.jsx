2e3979b978971ded7b031d55f8f3f0bd80b0130a
prime_jd <ravishankartripathi2002@gmail.com>
2024-03-31 15:39:57 +0530

    update footer

--- 09_viteReact/src/Components/Footer/Footer.jsx
+++ 09_viteReact/src/Components/Footer/Footer.jsx
@@ -3,22 +3,22 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer className="bg-white border-y">
+        <footer className="bg-gray-800 border-y">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <Link to="/" className="flex items-center">
                             <img
                                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP_Xc1CD4iP8oYZxM3zB4oOy47TqAvw5Xzk5_O0XgshzthrN0&s"
-                                className=" rounded-full mr-3 h-16"
+                                className=" rounded-full mr-3 h-16 border-2 border-white"
                                 alt="Logo"
                             />
                         </Link>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
-                            <ul className="text-gray-500 font-medium">
+                            <h2 className="mb-6 text-sm font-semibold text-white uppercase">Resources</h2>
+                            <ul className="text-white font-medium">
                                 <li className="mb-4">
                                     <Link to="/" className="hover:underline">
                                         Home
@@ -32,8 +32,8 @@ export default function Footer() {
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
-                            <ul className="text-gray-500 font-medium">
+                            <h2 className="mb-6 text-sm font-semibold text-white uppercase">Follow us</h2>
+                            <ul className="text-white font-medium">
                                 <li className="mb-4">
                                     <a
                                         href="https://github.com/prime-jd"
@@ -55,8 +55,8 @@ export default function Footer() {
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
-                            <ul className="text-gray-500 font-medium">
+                            <h2 className="mb-6 text-sm font-semibold text-white uppercase">Legal</h2>
+                            <ul className="text-white font-medium">
                                 <li className="mb-4">
                                     <Link to="#" className="hover:underline">
                                         Privacy Policy
@@ -73,7 +73,7 @@ export default function Footer() {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center">
+                    <span className="text-sm text-white sm:text-center">
                         © 2023
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
                             hiteshchoudhary
@@ -81,7 +81,7 @@ export default function Footer() {
                         . All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-                        <Link to="#" className="text-gray-500 hover:text-gray-900">
+                        <Link to="#" className="text-white hover:text-gray-900">
                             <svg
                                 className="w-4 h-4"
                                 aria-hidden="true"
@@ -97,7 +97,7 @@ export default function Footer() {
                             </svg>
                             <span className="sr-only">Facebook page</span>
                         </Link>
-                        <Link to="#" className="text-gray-500">
+                        <Link to="#" className="text-white">
                             <svg
                                 className="w-4 h-4"
                                 aria-hidden="true"
