c4355b37c8a1ea16e9bd319bb2a30aff934d77a1
prime_jd <ravishankartripathi2002@gmail.com>
2024-03-31 14:26:08 +0530

    pr

--- 09_viteReact/src/main.jsx
+++ 09_viteReact/src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Components/Home/Home.jsx'
+import App from './App.jsx'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element = {<Layout />}>
       <Route path='' element= {<Home />} />
+      <Route path='todo' element= {<App />} />
     </Route>
   )
 )
