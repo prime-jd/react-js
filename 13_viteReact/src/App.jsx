2a7449a8fef7f1ead0e5dcd4b117daf34c2610c4
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-01 04:37:43 +0530

    modify

--- 13_viteReact/src/App.jsx
+++ 13_viteReact/src/App.jsx
@@ -2,33 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Provider } from 'react-redux'
+import { Store } from './App/Store'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+import TodoSlice from './Features/Todo/TodoSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Provider store={Store}>
+      <AddTodo />
+      <Todos />
+    </Provider>
   )
 }
 
