45c6d16b9c7d2b8bdda59fd791728453931250d5
prime_jd <ravishankartripathi2002@gmail.com>
2024-03-31 15:41:07 +0530

    u[date header

--- 09_viteReact/src/Components/Header/Header.jsx
+++ 09_viteReact/src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 export default function Header() {
     return (
         <header className="shadow sticky z-50 top-0">
-            <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5">
+            <nav className="bg-gray-800 border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img 
@@ -16,7 +16,7 @@ export default function Header() {
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="#"
-                            className="text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-white hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
                         </Link>
@@ -42,12 +42,12 @@ export default function Header() {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/about"
+                                <NavLink to="/todo"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 ${isActive ? "text-orange-400" : "text-white"} pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:p-0`
                                     }
                                 >
-                                    About
+                                   Todo 
                                 </NavLink>
                             </li>
                             <li>
