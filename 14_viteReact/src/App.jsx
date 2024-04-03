4207242bf84a19b51eec113dd7720dfb7eea6f7b
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-04 03:13:13 +0530

    ..

--- 14_viteReact/src/App.jsx
+++ 14_viteReact/src/App.jsx
@@ -2,14 +2,45 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appWrite/Auth'
+import { login,logout } from './store/authSlice'
+import { Outlet } from 'react-router-dom'
+import {Header,Footer} from './components'
+
 
 function App() {
   
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
- <div>My MEGA PROJECT</div>
-  )
+  useEffect(()=>{
+
+    authService.getCurrentUser()
+    .then((userData)=>{
+
+      if(userData)
+      dispatch(login({userData}))
+
+      else dispatch(logout())
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header />
+        header
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        footer
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
