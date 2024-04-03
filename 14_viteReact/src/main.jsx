996618f0a2eec718c508d3c50b36e38396383388
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-04 05:07:19 +0530

    provider store

--- 14_viteReact/src/main.jsx
+++ 14_viteReact/src/main.jsx
@@ -8,6 +8,9 @@ import store from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store} />
+    <Provider store={store}>
+       <App />
+    </Provider>
+    
   </React.StrictMode>,
 )
