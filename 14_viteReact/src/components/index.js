0d5d78d97f3d589ea01281ed995cd8933ff55f16
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-04 05:07:38 +0530

    index.js

--- 14_viteReact/src/components/index.js
+++ 14_viteReact/src/components/index.js
@@ -1,4 +1,4 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
+import Header from './Header/Header';
+import Footer from './Footer/Footer'
 
 export default {  Header,Footer }
