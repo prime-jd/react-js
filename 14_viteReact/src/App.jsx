d8d4227b2bbe723edabe296fb4f7dc93d2264b91
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-03 21:47:32 +0530

    mega project using appwrite

---
+++ 14_viteReact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+ <div>My MEGA PROJECT</div>
+  )
+}
+
+export default App
