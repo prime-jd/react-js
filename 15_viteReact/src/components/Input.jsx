a95dfbd33fb2db30607597584812faf10e880490
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-13 02:49:55 +0530

    Input component

--- 15_viteReact/src/components/Input.jsx
+++ 15_viteReact/src/components/Input.jsx
@@ -1,25 +1,20 @@
-import React, { useId } from 'react'
+import React, {useId} from 'react';
+ // Import useId hook from @reach/auto-id
 
-
-function Input({
-    label,
-    type,
-    placeholder,
-    ...props
-},ref) {
-    const id = useId()
+const Input = React.forwardRef(({ label, type, placeholder, ...props }, ref) => {
+    const id = useId();
     return (
         <div>
-          {label && <label className='' htmlFor={id} >
-            {label}</label>}  
-
-          <input 
-          type = {type} 
-          id={id}
-          ref = {ref}
-           ></input>
+            {label && <label htmlFor={id}>{label}</label>}
+            <input
+                type={type}
+                id={id}
+                placeholder={placeholder}
+                ref={ref} // Forward the ref to the input element
+                {...props} // Spread any additional props
+            />
         </div>
-    )
-}
+    );
+});
 
-export default React.forwardRef(Input)
+export default Input;
