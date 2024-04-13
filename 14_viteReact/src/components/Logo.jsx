758d3bd3771735a427a60fa85793b9c6d0a8c95d
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-13 20:30:56 +0530

    logo component

--- 14_viteReact/src/components/Logo.jsx
+++ 14_viteReact/src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Logo() {
-    return (
-        <></>
-    )
+function Logo({width = '100px'}) {
+  return (
+    <div>Logo</div>
+  )
 }
 
 export default Logo
