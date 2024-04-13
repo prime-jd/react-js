f6cd11698e6d814d8c201b53624c45a8ada5722a
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-13 20:30:08 +0530

    App component

--- 14_viteReact/src/App.jsx
+++ 14_viteReact/src/App.jsx
@@ -1,44 +1,34 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import authService from './appWrite/Auth'
-import { login,logout } from './store/authSlice'
+import './App.css'
+import authService from "./appWrite/Auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-
 
 function App() {
-  
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
 
-  useEffect(()=>{
-
+  useEffect(() => {
     authService.getCurrentUser()
-    .then((userData)=>{
-
-      if(userData)
-      dispatch(login({userData}))
-
-      else dispatch(logout())
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
     })
-    .finally(()=>setLoading(false))
-  },[])
-
-
-  return (!loading) ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
-      
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
         <Header />
-        
         <main>
-          {/* <Outlet /> */}
+        TODO:  <Outlet />
         </main>
-
         <Footer />
       </div>
     </div>
