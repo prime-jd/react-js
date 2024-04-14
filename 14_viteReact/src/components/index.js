89ff69855d123b654a7b6b0fa4af64951d2b0185
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-14 17:52:25 +0530

    index imports

--- 14_viteReact/src/components/index.js
+++ 14_viteReact/src/components/index.js
@@ -2,7 +2,7 @@ import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
-import Logo from "/Logo";
+import Logo from "./Logo.jsx"
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
