51f77b062d137afeb28759ce6fa704d370afca2e
prime_jd <ravishankartripathi2002@gmail.com>
2024-06-16 20:46:17 +0530

    env

--- 14_viteReact/src/main.jsx
+++ 14_viteReact/src/main.jsx
@@ -35,7 +35,7 @@ const router = createBrowserRouter([
             ),
         },
         {
-            path: "signup",
+            path: "/signup",
             element: (
                 <AuthLayout authentication={false}>
                     <Signup />
